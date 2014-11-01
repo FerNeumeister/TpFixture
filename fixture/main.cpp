e0276bb4bbed089ebeecb2907f672fb5ce0cd026
FerNeumeister <ferni1556@gmail.com>
2014-11-01 20:32:41 -0300

    Simulacion con errores

--- fixture/main.cpp
+++ fixture/main.cpp
@@ -13,20 +13,21 @@ using namespace std;
 
 int Numeroseleccionado;
 
-typedef struct{
+typedef struct{   // La estructura "Tipo Partido"
     int local;
     int visitante;
 }Partido;
 
-typedef struct{
+typedef struct{  // La estructura "Tipo Fecha"
     Partido partidos[MAX_PARTIDOS];
     int cantidadPartidos;
+    bool laFechaFueJugada;
 
 }Fecha;
 
-typedef struct {
-Fecha fechas [MAX_FECHAS];
-int cantidadFechas;
+typedef struct {  // Estructura de "Tipo Fixture"
+    Fecha fechas [MAX_FECHAS];
+    int cantidadFechas;
 
 }Fixture;
 
@@ -35,14 +36,14 @@ typedef struct{
     char nombre [31];
     int paramA;
     int paramB;
+    int puntos;
 } Equipo;
 
-
-// tipo verctor enteros
 typedef struct{
     Equipo equipos[MAX_EQUIPOS];
     int n;
 }Equipos;
+
 
 void GuardarEquipos(Equipos &equipos){
 
@@ -90,6 +91,7 @@ void GenerarInterfaz (){
 
 }
 
+
 void GenerarFixture(const Equipos &equipos, Fixture &fixture){
 
 
@@ -118,9 +120,72 @@ void GenerarFixture(const Equipos &equipos, Fixture &fixture){
 
 }
 
-void SimularPartido(){
-cout << "Simulando Partido"<< endl;
+
+void SimularPartido(Equipo equipos, Fixture fixture){
+
+int i=0;
+int j=0;
+int x=0;
+char continuar=a;
+
+
+cout << "Ingrese una fecha: "<< endl;
+
+cin >> fixture.fechas[j];
+
+if (fixture.fechas[j].laFechaFueJugada=True){
+
+        cout<< "La fecha"<< j << "ya fue simulada. ¿Continuar? S/N" endl;
+        cin>> continuar;
+
+        if(continuar==S||s){
+
+            cout<< "estamos Simulando viejitaaa"<< ;
+        }
+        else{
+            exit (1);
+        }
 }
+else{
+
+
+    for(x;x<j+1;x++){
+
+        for(i;i<equipos.n/2;i++){
+
+            simularPartido(equipos[i].paramA,equipos[i].paramB,equipos[i+1].paramA,equipos[i+1].paramB)
+
+            if (goleslocal==golesVisitante){
+
+                    equipos[i].puntos+1;
+                    equipos[i+1].puntos+1;
+            }
+            if (goleslocal>golesVisitante){
+
+                    equipos[i].puntos+3
+
+            }
+            else{
+                    equipos[i+1].puntos+3
+            }
+        }
+
+        fixture.fechas[j].laFechaFueJugada=true
+    }
+
+}
+
+
+
+
+
+
+//for (i;i<equipos.n/2;i){
+
+  //  simularPartido(equipos.equipos[i].paramA,equipos.equipos[i].paramB,equipos.equipos[i++].paramA,equipos.equipos[i++].paramB,);
+
+}
+
 
 void SeleccionDeOpcion(int Numeroseleccionado,const Equipos &equipos, Fixture &fixture){
 
@@ -131,12 +196,33 @@ void SeleccionDeOpcion(int Numeroseleccionado,const Equipos &equipos, Fixture &f
     else {
         if (Numeroseleccionado==2){
 
-    SimularPartido();
+            SimularPartido(equipos, fixture)
 
 
         }
     }
     }
+
+
+void InicializarSimulacion (Fixture fixture){
+    int p;
+
+    for (p=0;p<MAX_FECHAS;p++){
+
+        fixture.fechas[p].laFechaFueJugada=false;
+    }
+
+
+}
+
+void InicializarPuntos (Equipo equipos){
+    int p;
+
+    for (p=0;p<MAX_EQUIPOS;p++){
+
+       equipos[p].puntos=0;
+    }
+}
 
 
 int main()
@@ -145,9 +231,14 @@ int main()
     srand (time(NULL));
     Equipos equipos;
 
+
     GuardarEquipos(equipos);
 
     GenerarInterfaz();
+
+    InicializarPuntos(equipos)
+
+    InicializarSimulacion(fixture);
 
     SeleccionDeOpcion(Numeroseleccionado,equipos,fixture);
 
